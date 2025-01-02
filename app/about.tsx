7208cbbf791f6425fb0b526344a125f701a07225
Ercan Öztürk <ercanozturk788@gmail.com>
2025-01-02 12:09:58 +0300

    NativeWind

---
+++ app/about.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function About() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>About Page</Text>
+    </View>
+  );
+}
